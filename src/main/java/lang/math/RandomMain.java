2b066fbe0446e06d93dfbf364f3cbf3e69ae59e6
unknown <seo970713@gmail.com>
2024-07-04 00:57:57 +0900

    wrapepr end

--- src/main/java/lang/math/RandomMain.java
+++ src/main/java/lang/math/RandomMain.java
@@ -4,7 +4,8 @@ import java.util.Random;
 
 public class RandomMain {
     public static void main(String[] args) {
-        Random random = new Random();
+//        Random random = new Random();
+        Random random = new Random(1);  // seed가 같으면 Random의 결과가 같다.
 
         int randomInt = random.nextInt();
         System.out.println("randomInt = " + randomInt);
@@ -14,5 +15,12 @@ public class RandomMain {
 
         boolean randomBoolean = random.nextBoolean();
         System.out.println("randomBoolean = " + randomBoolean);
+
+        // 범위 조회
+        int randomRange1 = random.nextInt(10);// 0 ~ 9 까지 출력
+        System.out.println("0 ~ 9: = " + randomRange1);
+
+        int randomRange2 = random.nextInt(10) + 1;// 1 ~ 10 까지 출력
+        System.out.println("1 ~ 10 = " + randomRange2);
     }
 }
