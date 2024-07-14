d8cdcb265b33dd9be36026025ae2dd208e796c98
unknown <seo970713@gmail.com>
2024-07-14 23:52:20 +0900

    eunm

---
+++ src/main/java/enumeration/ref1/ClassRefMain1.java
@@ -0,0 +1,16 @@
+package enumeration.ref1;
+
+import enumeration.ex2.ClassGrade;
+
+public class ClassRefMain1 {
+
+    public static void main(String[] args) {
+        System.out.println("class BASIC = "  + enumeration.ex2.ClassGrade.BASIC.getClass());
+        System.out.println("class GOLD = "  + enumeration.ex2.ClassGrade.GOLD.getClass());
+        System.out.println("class DIAMOND = "  + enumeration.ex2.ClassGrade.DIAMOND.getClass());
+
+        System.out.println("ref BASIC = " + enumeration.ex2.ClassGrade.BASIC);
+        System.out.println("ref GOLD = " + enumeration.ex2.ClassGrade.GOLD);
+        System.out.println("ref DIAMOND = " + ClassGrade.DIAMOND);
+    }
+}
