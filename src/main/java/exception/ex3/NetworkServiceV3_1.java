f4a7fdb2ca0f4481cc8436195cfb0a744fdbb18f
unknown <seo970713@gmail.com>
2024-08-20 00:42:30 +0900

    thorw

---
+++ src/main/java/exception/ex3/NetworkServiceV3_1.java
@@ -0,0 +1,28 @@
+package exception.ex3;
+
+
+import exception.ex3.exception.ConnectExceptionV3;
+import exception.ex3.exception.SendExceptionV3;
+
+public class NetworkServiceV3_1 {
+
+    public void sendMessage(String data){
+        String address = "http://example.com";
+        NetworkClientV3 client = new NetworkClientV3(address);
+        client.initError(data); // 추가
+
+        try {
+            client.connect();
+            client.send(data);
+        } catch (ConnectExceptionV3 e) {
+            System.out.println("[연결 오류] 주소: " + e.getAddress() + ", 메시지: " + e.getMessage());
+        } catch (SendExceptionV3 e) {
+            System.out.println("[전송 오류] 전송 데이터: " + e.getSendData() + ", 메시지: " + e.getMessage());
+        } finally {
+            // catch 에서 잡을 순 없어도 finally 로 자원 마무리는 함.
+            // finally 후 catch 실행
+            client.disconnect();
+        }
+    }
+
+}
---
+++ src/main/java/exception/ex3/NetworkServiceV3_2.java
@@ -0,0 +1,31 @@
+package exception.ex3;
+
+
+import exception.ex3.exception.ConnectExceptionV3;
+import exception.ex3.exception.NetworkClientExceptionV3;
+import exception.ex3.exception.SendExceptionV3;
+
+public class NetworkServiceV3_2 {
+
+    public void sendMessage(String data){
+        String address = "http://example.com";
+        NetworkClientV3 client = new NetworkClientV3(address);
+        client.initError(data); // 추가
+
+        try {
+            client.connect();
+            client.send(data);
+        } catch (ConnectExceptionV3 e) {
+            System.out.println("[연결 오류] 주소: " + e.getAddress() + ", 메시지: " + e.getMessage());
+        } catch (NetworkClientExceptionV3 e) {
+            System.out.println("[네트워크 오류] 메시지: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("[알 수 없는 오류] 메시지: " + e.getMessage());
+        } finally {
+            // catch 에서 잡을 순 없어도 finally 로 자원 마무리는 함.
+            // finally 후 catch 실행
+            client.disconnect();
+        }
+    }
+
+}
